9e32c26ee84fa5bd652f0e0b61865c1da37a8d7c
Master-mohit <mohitmansigupta9098@gmail.com>
2026-07-16 20:00:44 +0530

    Use MongoDB Atlas connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ConnectDB = async()=> {
-    mongoose.connect('mongodb://localhost:27017/SAC');
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-
-
-
-module.exports = ConnectDB;
+module.exports = connectDB;
